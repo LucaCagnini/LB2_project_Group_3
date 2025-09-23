f5548868b25ba1b54b379300b79855312f3d74b9
giacomo-timelli <timelli.giacomo@gmail.com>
2025-09-23 15:10:02 +0200

    repository shuffling, everyday i'm shuffling -jackie

--- Data_Collection/cluster.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import Bio
-import shlex
-
-subprocess.run(shlex.split("mmseqs easy-cluster /home/um13/lab2_project/LB2_project_Group_3/Data_Collection/negative_dataset.fasta /home/um13/lab2_project/LB2_project_Group_3/Data_Collection/neg_cluster/neg tmp --min-seq-id 0.3 -c 0.4 --cov-mode 0 --cluster-mode 1"), stdout=subprocess.DEVNULL)
-subprocess.run(shlex.split("mmseqs easy-cluster /home/um13/lab2_project/LB2_project_Group_3/Data_Collection/positive_dataset.fasta /home/um13/lab2_project/LB2_project_Group_3/Data_Collection/pos_cluster/pos tmp --min-seq-id 0.3 -c 0.4 --cov-mode 0 --cluster-mode 1"), stdout=subprocess.DEVNULL)
-
